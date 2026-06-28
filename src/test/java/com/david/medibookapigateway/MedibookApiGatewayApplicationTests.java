97442230d0224a65ee0cdd5badfa69c7e1a86c61
David-282 <afolabidavid94@gmail.com>
2026-06-28 19:50:43 +0100

    feat: project file structure

---
+++ src/test/java/com/david/medibookapigateway/MedibookApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.medibookapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedibookApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
